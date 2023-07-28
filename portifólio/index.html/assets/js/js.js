c8eeebcdad638e28a5d5ec3ff895247e06044641
vitória <129601536+vigabriele@users.noreply.github.com>
2023-07-27 22:17:40 -0300

    27/07/23
    27/07/23

---
+++ portifólio/index.html/assets/js/js.js
@@ -0,0 +1,11 @@
+const abrirJanela = document.querySelectorAll('.janela.abrir')
+
+abrirJanela.forEach((abrir) => {
+    abrir.addEventListener('click', (e) =>)
+    const janela = abrir.parentElement
+    const isOpen = janela.classList.contains('open')
+    
+    if (isOpen) {
+        janela.classList.remove('open')sds
+    }
+});
